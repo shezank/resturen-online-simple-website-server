50b2a9bccbb523cf8db9f0f7bf64c8adae70c96e
shezank <mdfarhankobirkhan1@gmail.com>
2023-11-08 02:23:52 +0600

    vercel include update again

--- index.js
+++ index.js
@@ -4,21 +4,15 @@ require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
-const corsConfig = {
-    origin: '',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-  }
-  
-  app.use(cors({
+app.use(cors({
     origin: [
-      'https://kasmeri-resturent.web.app',
-      'https://kasmeri-resturent.firebaseapp.com'
+        'http://localhost:5173',
+        'https://kasmeri-resturent.web.app',
+        'https://kasmeri-resturent.firebaseapp.com'
     ],
     credentials: true
-  }));
-  app.use(express.json());
-  app.use(cors(corsConfig));
+}));
+app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iyzg5.mongodb.net/?retryWrites=true&w=majority`;
