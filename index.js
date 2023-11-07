38cc6e0720502372dc8afd445362456b37a05796
shezank <mdfarhankobirkhan1@gmail.com>
2023-11-08 01:22:26 +0600

    added prodcut count , update product , deleted orders

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iyzg5.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,6 +29,8 @@ async function run() {
         await client.connect();
 
         const userCollection = client.db("Kasmeri_Resturent").collection('users');
+        const productCollection = client.db("Kasmeri_Resturent").collection('products');
+        const orderCollection = client.db("Kasmeri_Resturent").collection('orders');
 
 
         app.get('/', (req, res) => {
@@ -42,12 +44,89 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/products', async (req, res) => {
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            const result = await productCollection.find()
+            .skip(page * size)
+            .limit(size)
+            .toArray();
+            res.send(result);
+        })
+
+        app.get('/productsCount', async (req, res) => {
+            const count = await productCollection.estimatedDocumentCount()
+            res.send({count});
+        })
+
+        app.get('/products/:uid', async (req, res) => {
+            const user = req.params.uid;
+            const userUid = { userProduct: user };
+            const result = await productCollection.find(userUid).toArray();
+            res.send(result);
+        })
+
+        app.put('/products/:uid', async (req, res) => {
+            const uid = req.params.uid;
+            const filter = {userProduct: uid};
+            const user = req.body; 
+            const options = { upsert: true };
+            const updateUser = {
+                $set: {
+                    userProduct: user.userProduct, 
+                    foodName: user.foodName, 
+                    foodImg: user.foodImg, 
+                    foodCategory: user.foodCategory, 
+                    quantity: user.quantity, 
+                    price: user.price, 
+                    orderOwnerName: user.orderOwnerName, 
+                    ownerEmail: user.ownerEmail, 
+                    country: user.country, 
+                    description: user.description
+                },
+              };
+            const result = await productCollection.updateOne(filter, updateUser, options);
+            res.send(result);
+        })
+
+        app.get('/products/details/:id', async (req, res) => {
+            const id = req.params.id;
+            const userId = { _id: new ObjectId(id) };
+            const result = await productCollection.findOne(userId);
+            res.send(result);
+        })
+
+
+        app.post('/products', async (req, res) => {
+            const newUser = req.body;
+            const result = await productCollection.insertOne(newUser);
+            res.send(result);
+        })
+        app.post('/orders', async (req, res) => {
+            const product = req.body;
+            const result = await orderCollection.insertOne(product);
+            res.send(result);
+        })
+
+        app.get('/orders/:email', async(req,res)=>{
+            const email = req.params.email;
+            const query = {orderEmail: email};
+            const result = await orderCollection.find(query).toArray();
+            res.send(result)
+        })
+        app.delete('/orders/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await orderCollection.deleteOne(query);
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
